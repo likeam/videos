5144e9da1e369242fd26bb43df4d30d8cadf04d8
AbdulRehman <likeam99@gmail.com>
2022-10-11 06:14:40 +0500

    Initial Setup

---
+++ src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+class SearchBar extends React.Component {
+        render() {
+            return (
+            <div>
+            Search Bar
+            </div>
+        )
+    }
+}
+
+SearchBar.propTypes = {
+
+}
+
+export default SearchBar
